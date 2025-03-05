a76c8de623463124305fca0cfd7ee4d1882340f8
ndolocr <crndolo@gmail.com>
2025-03-05 20:24:47 +0300

    Subtruction Function

--- calc.py
+++ calc.py
@@ -3,4 +3,10 @@ def add(num_1, num_2):
     print(f"The sum of {num_1} + {num_2 } = {answer}")
 
 add(2, 3)
-    
+
+# Gladys 
+def subtruction(num_1, num_2):
+    answer = num_1 - num_2
+    print(f"The difference of {num_1} - {num_2 } = {answer}")
+
+subtruction(2, 3)
