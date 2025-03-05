e3eb58ac297f857fce6603e9a5d35c1a1332bbfb
ndolocr <crndolo@gmail.com>
2025-03-05 20:07:59 +0300

    Working on the division function

--- calc.py
+++ calc.py
@@ -3,4 +3,11 @@ def add(num_1, num_2):
     print(f"The sum of {num_1} + {num_2 } = {answer}")
 
 add(2, 3)
-    
+
+# Ian
+def divide(num_1, num_2):
+    answer = num_1 / num_2
+    print(f"The division of {num_1} / {num_2 } = {answer}")
+
+
+divide(2, 3)
